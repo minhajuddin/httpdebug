02195694bed4f0ae55b3f66d4d6305596b75231f
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2015-04-10 10:57:39 +0530

    init

---
+++ app.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+		fmt.Println(string(bytes))
+		fmt.Println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+
+		fmt.Fprintln(w, "<h1>HTTP Debug</h1>")
+		fmt.Fprintln(w, "<pre>"+string(bytes)+"</pre>")
+
+	})
+	fmt.Println("Starting debug server at http://localhost:4000/ ..")
+	http.ListenAndServe(":4000", nil)
+}
